3a45458d90267b22f9d4311f8d4cb973876c9ba2
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2020-02-14 14:09:45 +0800

    init

---
+++ src/main/java/com/msig/myinfo/MyinfoApplication.java
@@ -0,0 +1,13 @@
+package com.msig.myinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msig/myinfo/MyinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msig.myinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
